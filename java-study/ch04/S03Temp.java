4f4a99ffafefcc175d4a64f4fc04efd3349a1a4d
woojoowoobi <lll5451@gmail.com>
2023-06-30 14:51:38 +0900

    0630 복습

--- java-study/ch04/S03Temp.java
+++ java-study/ch04/S03Temp.java
@@ -17,21 +17,21 @@ public class S03Temp {
         // 1 / 1
         // System.out.println(-num / (num + (num == -1 ? 2 : 1)));
 
-        // switch(-num / (2 + num)){
-        // case -1:
-        // System.out.println("잘못 입력했다");
-        // return;
-        // default:
-        // System.out.println("잘 입력했다.");
-        // }
+    //     switch(-num / (2 + num)){
+    //     case -1:
+    //     System.out.println("잘못 입력했다");
+    //     return;
+    //     default:
+    //     System.out.println("잘 입력했다.");
+    //     }
 
-        // switch (num >= 0 ? 1 : 0) {
-        //     case 0:
-        //         System.out.println("잘못 입력했다");
-        //         return;
-        //     default:
-        //         System.out.println("잘 입력했다.");
-        // }
+    //     switch (num >= 0 ? 1 : 0) {
+    //         case 0:
+    //             System.out.println("잘못 입력했다");
+    //         return;
+    //         default:
+    //             System.out.println("잘 입력했다.");
+    //     }
 
     }
 }
