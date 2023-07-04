8164d2eb3360a5c8fe44450b8bb95feb6da079ee
woojoowoobi <lll5451@gmail.com>
2023-07-04 15:16:57 +0900

    객체지향 언어 전

---
+++ java-study/house/Home08.java
@@ -0,0 +1,16 @@
+package house;
+
+import java.util.Arrays;
+
+public class Home08 {
+        public static void main(String[] args) {
+            //            01234
+            String str = "ABCDE";
+            char ch = str.charAt(2); // 2번에 있는 문자를 불러온다
+            String str2 = str.substring(1,4); // 1~4사이 문자열을 물러온다
+            String str3 = str.substring(1); // 1~4 문자열 불러온다
+            // String str3 = str.substring(1, str.length());  위와 같은 내용
+            System.out.println(str3);
+      
+        }
+}
